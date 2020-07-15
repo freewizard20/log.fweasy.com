4a1849285f06f7c6a22cc887f53b6c15b662e135
fwcorp <gseongkyu79@gmail.com>
2020-07-15 19:24:52 +0900

    added currency

--- src/components/PriceTable.js
+++ src/components/PriceTable.js
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
+const getPrice = () => {
+    return axios.get("http://ap-south-1.linodeobjects.com/fweasy/price.js")
+}
+
+const getCurrency = () => {
+    return axios.get("https://api.exchangeratesapi.io/latest?symbols=USD,KRW");
+}
+
 export default function PriceTable() {
     const [load, setLoad] = useState(false);
     const [priceList, setPriceList] = useState([]);
+    const [currency, setCurrency] = useState("");
 
     useEffect(() => {
         if (load === false) {
-            axios.get('http://ap-south-1.linodeobjects.com/fweasy/price.js')
-                .then(({ data }) => {
-                    setPriceList(priceList.concat(data));
-                    setLoad(true);
-                })
-
+            axios
+                .all([getPrice(), getCurrency()])
+                .then(
+                    axios.spread((price, currency) => {
+                        setPriceList(priceList.concat(price));
+                        setLoad(true);
+                        setCurrency((currency.data.rates.KRW / currency.data.rates.USD).toFixed(2))
+                    })
+                )
         }
     }, [])
 
     return (
         <div>
-
+            {currency}
         </div>
     )
 }
